3ec201d8d46c56dcd57d3cdb639307544bd56a71
Brendan Tierney <tiernebre@gmail.com>
2021-02-09 22:42:47 -0700

    Test New Change (#12)
    * new change
    
    * rewrite

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>Testing a brand new changeset for the Zone Blitz Web Application.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
